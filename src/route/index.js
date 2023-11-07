c35dacfff3e1841ac0cf1e971b4637a19df5ff6b
StanislavRadas <stasradysh@gmail.com>
2023-11-07 15:58:55 +0100

    homework

--- src/route/index.js
+++ src/route/index.js
@@ -9,11 +9,7 @@ const router = express.Router()
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  //            ↙ cюди вводимо назву файлу з сontainer
   res.render('index', {})
-  //                  ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
